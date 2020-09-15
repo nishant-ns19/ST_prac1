65bb4611de350fee172ca906944d64f64da9a610
Nishant <nishant.jai@outlook.com>
2020-09-15 22:01:09 +0530

    Updated Class

--- src/main/java/com/nishant/demo/Shop.java
+++ src/main/java/com/nishant/demo/Shop.java
@@ -28,21 +28,23 @@ public class Shop {
     }
 
     public boolean addNewItem(String name, Integer initialQuantity, Integer perItemCost) throws Exception {
-        if (perItemCost <= MIN_COST) {
+        if (perItemCost < MIN_COST) {
             throw new Exception("Invalid per item cost");
         }
-        if (initialQuantity <= MIN_INITIAL_QUANTITY) {
+        if (initialQuantity < MIN_INITIAL_QUANTITY) {
             throw new Exception("Invalid quantity");
         }
-        if (!isAlphaNumeric(name)) {
-            throw new Exception("Invalid name");
+        String words[] = name.split("\\s+");
+        if(words.length < MIN_WORDS || words.length > MAX_WORDS) {
+            throw new Exception("Invalid item name length");
+        }
+        for(String word : words){
+            if (!isAlphaNumeric(word)) {
+                throw new Exception("Invalid name");
+            }
         }
         if (isPresent(name)) {
             throw new Exception("Existing item can't be added again");
-        }
-        int countWords = name.split("\\s+").length;
-        if(countWords < MIN_WORDS) {
-            throw new Exception("Invalid name");
         }
         quantity.put(name, initialQuantity);
         cost.put(name, perItemCost);
@@ -52,9 +54,6 @@ public class Shop {
     public int restockItem(String name, Integer quantity) throws Exception {
         if (quantity <= MIN_INITIAL_QUANTITY) {
             throw new Exception("Invalid quantity");
-        }
-        if (!isAlphaNumeric(name)) {
-            throw new Exception("Invalid name");
         }
         if (!isPresent(name)) {
             throw new Exception("Item not available for re-stocking");
@@ -67,14 +66,11 @@ public class Shop {
         int totalBill = 0;
         for (String itemName : cart.keySet()) {
             int itemQuantity = cart.get(itemName);
-            if (!isAlphaNumeric(itemName)) {
-                throw new Exception("Invalid name: " + itemName);
-            }
             if (itemQuantity <= MIN_INITIAL_QUANTITY) {
                 throw new Exception("Invalid quantity for: " + itemName);
             }
             if (!isPresent(itemName) || quantity.get(itemName) <= MIN_INITIAL_QUANTITY) {
-                throw new Exception("Item not available: " + itemName);
+                throw new Exception("Item not available/Invalid item name : " + itemName);
             }
             if (quantity.get(itemName) < itemQuantity) {
                 throw new Exception("Insufficient stock for: " + itemName);
--- src/test/java/com/nishant/demo/ShopTest.java
+++ src/test/java/com/nishant/demo/ShopTest.java
@@ -8,22 +8,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTest {
 
     @Test
     void restockItem() throws Exception {
         Shop shop=new Shop();
-//        Exception exception = assertThrows(Exception.class, () -> shop.restockItem("he$$",1));
+        assertTrue(shop.addNewItem("abc b b b b b b b b b", 2, 2));
+//        Exception exception = assertThrows(Exception.class, () -> shop.addNewItem("abc n b b b b b%% b b b",2,2));
 //        assertEquals("Invalid name",exception.getMessage());
-//        shop.addNewItem("abc",2,2);
+
 //        assertEquals(3,shop.restockItem("abc",1));
-        HashMap<String,Integer> cart=new HashMap<>();
-//        Pair<String,Integer> newItem = new Pair("anbc", -1);
-        cart.put("anbc",-1);
-        Exception exception = assertThrows(Exception.class, () -> shop.buyItem(cart));
-        assertEquals("Invalid quantity for: anbc", exception.getMessage());
+//        HashMap<String,Integer> cart=new HashMap<>();
+////        Pair<String,Integer> newItem = new Pair("anbc", -1);
+//        cart.put("anbc",-1);
+//        Exception exception = assertThrows(Exception.class, () -> shop.buyItem(cart));
+//        assertEquals("Invalid quantity for: anbc", exception.getMessage());
     }
 }
